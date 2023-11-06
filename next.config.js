e5280e8f2c22aace31eb0db771a7a3764b009ad1
Muhamed Didovic <muhamed.didovic@gmail.com>
2023-11-06 11:15:56 +0100

    test increase timeout

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+	server: {
+		// Increase the timeout value (in milliseconds) as per your requirements
+		apiTimeout: 30000, // 30 seconds timeout
+	},
 }
 
 module.exports = nextConfig
