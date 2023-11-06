eb93135407e92e330c05d0f025c72212636673a0
Muhamed Didovic <muhamed.didovic@gmail.com>
2023-11-06 10:34:24 +0100

    add missing module

---
+++ utils/supabaseClient.js
@@ -0,0 +1,7 @@
+// utils/supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY;
+
+export const client = createClient(supabaseUrl, supabaseAnonKey);
